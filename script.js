b8273929a5a175867e1884b3ab31ce4a1ac326ef
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2023-12-21 00:39:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstChar(text) {
   // your code here
+	// for(let i=0; i<text.length; i++){
+		if(text[i]!=" "){
+			return text[0];
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
